625334c7c4060b47ee23e4ceb250f2723db3a399
kka491 <kkostrukova06@gmail.com>
2025-12-06 23:32:20 +0300

    buttons ux

--- Pypypy_Musor_F/src/components/Header/Header.tsx
+++ Pypypy_Musor_F/src/components/Header/Header.tsx
@@ -11,18 +11,21 @@ interface Props {
 export const Header: React.FC<Props> = ({ className }) => {
   const [isRatingOpen, setIsRatingOpen] = useState(false);
   return (
-		<>
-			<header className={cn("border-b", className)}>
-				<Container className="flex items-center justify-between py-8">
-					<img src="/logo.svg" alt="Logo" width={95} height={95} />
+    <>
+      <header className={cn("border-b", className)}>
+        <Container className="flex items-center justify-between py-8">
+          <img src="/logo.svg" alt="Logo" width={95} height={95} />
 
-					<ChartNoAxesColumnIncreasing onClick={() => setIsRatingOpen(true)} className="text-amber-400 w-24 h-24 " />
-				</Container>
-			</header>
-			<RatingModal
-				isOpen={isRatingOpen}
-				onClose={() => setIsRatingOpen(false)}
-			/>
-		</>
+          <ChartNoAxesColumnIncreasing
+            onClick={() => setIsRatingOpen(true)}
+            className="text-amber-400 w-24 h-24 cursor-pointer"
+          />
+        </Container>
+      </header>
+      <RatingModal
+        isOpen={isRatingOpen}
+        onClose={() => setIsRatingOpen(false)}
+      />
+    </>
   );
 };
--- Pypypy_Musor_F/src/components/shared/choice.tsx
+++ Pypypy_Musor_F/src/components/shared/choice.tsx
@@ -18,7 +18,7 @@ export const Choice: React.FC<React.PropsWithChildren> = () => {
         <div className="flex flex-col gap-4 max-w-2xs text-lg md:text-xl text-center">
           <p>Не знаешь куда выбросить мусор? Жми сюда!</p>
           <Button
-            className="h-16 md:h-20 rounded-3xl text-xl md:text-2xl text-slate-700"
+            className="h-16 md:h-20 rounded-3xl text-xl md:text-2xl text-slate-700 cursor-pointer"
             onClick={() => setIsModalOpen(true)}
           >
             Сфотографировать
@@ -27,7 +27,7 @@ export const Choice: React.FC<React.PropsWithChildren> = () => {
         <div className="flex flex-col gap-4 max-w-2xs text-lg md:text-xl text-center">
           <p>Уже знаешь куда выкинуть? Проверь себя и получи баллы!</p>
           <Button
-            className="h-16 md:h-20 rounded-3xl text-xl md:text-2xl text-slate-700"
+            className="h-16 md:h-20 rounded-3xl text-xl md:text-2xl text-slate-700 cursor-pointer"
             onClick={() => setIsQuizOpen(true)}
           >
             Проверить себя
